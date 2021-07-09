29bf9cbc0b3dac6e9db3b19817180a639961d8ce
溪桥 <756445638@qq.com>
2021-07-09 15:17:43 +0800

    add param bumber

--- snap7.go
+++ snap7.go
@@ -1,1 +1,19 @@
 package snap7go
+
+type ParamNumber = int
+
+const P_u16_LocalPort ParamNumber = 1
+const P_u16_RemotePort ParamNumber = 2
+const P_i32_PingTimeout ParamNumber = 3
+const P_i32_SendTimeout ParamNumber = 4
+const P_i32_RecvTimeout ParamNumber = 5
+const P_i32_WorkInterval ParamNumber = 6
+const P_u16_SrcRef ParamNumber = 7
+const P_u16_DstRef ParamNumber = 8
+const P_u16_SrcTSap ParamNumber = 9
+const P_i32_PDURequest ParamNumber = 10
+const P_i32_MaxClients ParamNumber = 11
+const P_i32_BSendTimeout ParamNumber = 12
+const P_i32_BRecvTimeout ParamNumber = 13
+const P_u32_RecoveryTime ParamNumber = 14
+const P_u32_KeepAliveTime ParamNumber = 15
