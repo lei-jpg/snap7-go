fb30cf6d91f5f7bb9fee3caa96eba11089407f0e
溪桥 <756445638@qq.com>
2021-07-09 12:26:43 +0800

    spell wrong

--- client.go
+++ client.go
@@ -12,10 +12,10 @@ type S7Object = C.ulong
 
 func Cli_GetCpuInfo(cli S7Object) (info TS7CpuInfo, err error) {
 	var code C.int = C.Cli_GetCpuInfo(cli, (*C.TS7CpuInfo)(unsafe.Pointer(&info)))
-	err = cliErrorTables[int(code)]
+	err = cliErrorsTable[int(code)]
 	return
 }
 
-var cliErrorTables = map[int]error{
+var cliErrorsTable = map[int]error{
 	0x001: errors.New("Error during PDU negotiation."),
 }
